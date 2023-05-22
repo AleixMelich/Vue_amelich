dee4b82f4a9dae5c3e5ad41449b8bf8e27e07085
«AleixMelich» <aleixmelich@iesebre.com>
2023-05-22 20:09:39 +0200

    Comensament vidio 9

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 20,
-            product: 'Equipacio FCB 22/23',
-            image: './assets/images/primera_equipacio.jpg',
-            inStock: true,
+            cart: 0,
+            product: 'Equipacio 22/23',
+            brand: 'FCB',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool','20% polyester'],
             variants: [
-                { id: 2234, color: 'Primera', image: './assets/images/primera_equipacio.jpg' },
-                { id: 2235, color: 'Segona', image: './assets/images/segona_equipacio.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/primera_equipacio.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/segona_equipacio.jpg', quantity: 0 },
              ]
         }
     },
@@ -16,8 +16,20 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
