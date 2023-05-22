acf968c1bf888e0a26e2380eea8354ce571946b1
«AleixMelich» <aleixmelich@iesebre.com>
2023-05-22 19:20:48 +0200

    Comensament video 7 vue

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Primera Equipacio FCB 22/23',
             image: './assets/images/primera_equipacio.jpg',
-            inventory: 100,
+            inStock: true,
             details: ['50% cotton', '30% wool','20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/primera_equipacio.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/segona_equipacio.jpg' },
+             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
