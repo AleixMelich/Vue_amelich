fca94296afdeb9e61e62df049ba3969ff3f6fa23
«AleixMelich» <aleixmelich@iesebre.com>
2023-05-22 18:31:07 +0200

    Comensament video 5 vue

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Primera Equipacio FCB 22/23',
-            image: './assets/images/primera_equipacio.jpg'
+            image: './assets/images/primera_equipacio.jpg',
+            inventory: 8
         }
     }
 })
